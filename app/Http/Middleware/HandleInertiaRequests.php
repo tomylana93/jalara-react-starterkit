cbcdbb501c9bd636ba32c5b460389d9b1c3dad1f
Tomy Maulana <stealthflanker93@gmail.com>
2026-09-17 18:12:37 +0700

    refactor(config): add strict types declaration to configuration files

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Override;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -14,6 +15,7 @@ class HandleInertiaRequests extends Middleware
      *
      * @var string
      */
+    #[Override]
     protected $rootView = 'app';
 
     /**
