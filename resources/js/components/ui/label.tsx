7cfa6a23f9d8278c1bddea0c452df2e6c7fd4248
Tomy Maulana <stealthflanker93@gmail.com>
2026-09-18 13:50:16 +0700

    feat(ui): add new components for stepper, switch, table, tabs, tags input, toggle group, textarea, and etc

--- resources/js/components/ui/label.tsx
+++ resources/js/components/ui/label.tsx
@@ -1,7 +1,6 @@
-import * as LabelPrimitive from "@radix-ui/react-label"
 import * as React from "react"
-
-import { cn } from "@/lib/utils"
+import { cn } from "cn"
+import { Label as LabelPrimitive } from "radix-ui"
 
 function Label({
   className,
@@ -11,7 +10,7 @@ function Label({
     <LabelPrimitive.Root
       data-slot="label"
       className={cn(
-        "text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
         className
       )}
       {...props}
