d27e863985155e6748cee159b2bd540156e61d80
Tomy Maulana <stealthflanker93@gmail.com>
2026-09-18 14:25:42 +0700

    feat(auth): replace welcome page with intended redirects

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -13,8 +13,6 @@ void createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     layout: (name) => {
         switch (true) {
-            case name === 'welcome':
-                return null;
             case name.startsWith('auth/'):
                 return AuthLayout;
             case name.startsWith('settings/'):
